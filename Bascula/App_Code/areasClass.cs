2209937cc1a9ec10034f82f4fad204d0d07b9b52
axel <axelflores23@gmail.com>
2018-05-16 10:30:15 -0600

    axel

---
+++ Bascula/App_Code/areasClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de areasClass
+/// </summary>
+namespace Bascula.App_Code
+{
+    public class areasClass
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String Descripcion { get; set; }
+        public int Status { get; set; }
+
+
+        public areasClass() { }
+
+        public areasClass(int pId, String pNombre, String pDescripcion, int pStatus)
+        {
+            this.Id = pId;
+            this.Nombre = pNombre;
+            this.Descripcion = pDescripcion;
+            this.Status = pStatus;
+        }
+    }
+}
---
+++ Bascula/App_Code/usuariosClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Usuarios
+/// </summary>
+namespace Bascula.App_Code
+{
+    public class usuariosClass
+    {
+        public int Id { get; set; }
+        public String Usuario { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public int Status { get; set; }
+        public String Contrsasena { get; set; }
+        public int Rol { get; set; }
+        public int AreaDesignada { get; set; }
+
+        public usuariosClass() { }
+
+        public usuariosClass(int pId, String pUsuario, String pNombre, string pApellido, int pStatus, String pContrasena, int pRol, int pArea)
+        {
+            this.Id = pId;
+            this.Usuario = pUsuario;
+            this.Nombre = pNombre;
+            this.Apellido = pApellido;
+            this.Status = pStatus;
+            this.Contrsasena = pContrasena;
+            this.Rol = pRol;
+            this.AreaDesignada = pArea;
+        }
+    }
+}
